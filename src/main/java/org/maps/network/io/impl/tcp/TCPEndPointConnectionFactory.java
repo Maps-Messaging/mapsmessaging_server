1ee4a9cc0e3792c2f721bb50ae4d4bd03ae2d931
Matthew Buckton <matthew@buckton.org>
2021-02-25 09:35:56 +1100

    clear out false positive socket life cycle. We need to keep the socket open, its not short lived and these classes vend sockets for use

--- src/main/java/org/maps/network/io/impl/tcp/TCPEndPointConnectionFactory.java
+++ src/main/java/org/maps/network/io/impl/tcp/TCPEndPointConnectionFactory.java
@@ -33,7 +33,7 @@ import org.maps.network.io.impl.SelectorLoadManager;
 public class TCPEndPointConnectionFactory extends EndPointConnectionFactory {
 
   // We need to open a socket, its a socket library so we can ignore this issue
-  @java.lang.SuppressWarnings("squid:S4818")
+  @java.lang.SuppressWarnings("squid:S4818, squid:S2095")
   @Override
   public EndPoint connect(EndPointURL url, SelectorLoadManager selector, EndPointConnectedCallback connectedCallback, EndPointServerStatus endPointServerStatus, List<String> jmxPath) throws IOException {
     SocketChannel channel = SocketChannel.open();
