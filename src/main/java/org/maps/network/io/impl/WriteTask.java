21739c3abacf2d2daa466a9eae916cb13f58bb39
Matthew Buckton <matthew@buckton.org>
2021-03-16 14:54:13 +1100

    revert changes

--- src/main/java/org/maps/network/io/impl/WriteTask.java
+++ src/main/java/org/maps/network/io/impl/WriteTask.java
@@ -105,17 +105,15 @@ public class WriteTask implements Selectable {
         while(count < coalesceSize && serverPacket != null){
           int startPos = packet.position();
           try {
-            System.err.println("Packing "+serverPacket);
             serverPacket.packFrame(packet);
             completedFrames.add(serverPacket);
             count++;
           }
           catch(BufferOverflowException overflow){
-            overflow.printStackTrace();
             selectorCallback.getEndPoint().incrementOverFlow();
             coalesceSize = count;
             packet.position(startPos);
-            ((ConcurrentLinkedDeque<ServerPacket>)outboundFrame).addFirst(serverPacket);
+            ((LinkedList<ServerPacket>)outboundFrame).addFirst(serverPacket);
             serverPacket = null;
             count = coalesceSize;
           }
