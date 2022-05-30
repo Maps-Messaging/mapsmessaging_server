5129bdaad7b8e52944c3652621de6957f48f0a08
Matthew Buckton <matthew@buckton.org>
2022-05-30 14:01:01 +1000

    created a generic udp session manager to remove duplicate code where this is required

--- src/main/java/io/mapsmessaging/network/protocol/impl/semtech/json/StatPacket.java
+++ src/main/java/io/mapsmessaging/network/protocol/impl/semtech/json/StatPacket.java
@@ -1,16 +1,39 @@
 package io.mapsmessaging.network.protocol.impl.semtech.json;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class StatPacket {
 
+  @Getter
+  @Setter
   private String time;
+  @Getter
+  @Setter
   private double lati;
+  @Getter
+  @Setter
   private double longitude;
+  @Getter
+  @Setter
   private long alti;
+  @Getter
+  @Setter
   private long rxnb;
+  @Getter
+  @Setter
   private long rxok;
+  @Getter
+  @Setter
   private long rxfw;
+  @Getter
+  @Setter
   private double ackr;
+  @Getter
+  @Setter
   private long dwnb;
+  @Getter
+  @Setter
   private long txnb;
 }
 
