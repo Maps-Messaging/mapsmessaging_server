a3d6b1e5fcae2fdd009a819f80ea765326c0f8ff
Matthew Buckton <matthew@buckton.org>
2022-08-17 15:06:44 +1000

    ensure the observe can be cancelled

--- src/test/java/io/mapsmessaging/network/protocol/impl/coap/CoapObserverTest.java
+++ src/test/java/io/mapsmessaging/network/protocol/impl/coap/CoapObserverTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapObserveRelation;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.elements.exception.ConnectorException;
 import org.junit.jupiter.api.Assertions;
@@ -20,7 +21,6 @@ class CoapObserverTest extends BaseCoapTest {
     client.observe(new CoapHandler() {
       @Override
       public void onLoad(CoapResponse coapResponse) {
-        System.err.println("onload>>"+coapResponse);
         counter.incrementAndGet();
       }
 
@@ -40,6 +40,49 @@ class CoapObserverTest extends BaseCoapTest {
       count++;
     }
     Assertions.assertEquals(4, counter.get());
+    client.shutdown();
+  }
+
+  @Test
+  void testObserverAndClear() throws ConnectorException, IOException, InterruptedException {
+    CoapClient client = new CoapClient(getUri());
+    AtomicLong counter = new AtomicLong(0);
+    CoapObserveRelation observeRelation = client.observe(new CoapHandler() {
+      @Override
+      public void onLoad(CoapResponse coapResponse) {
+        counter.incrementAndGet();
+      }
+
+      @Override
+      public void onError() {
+        System.err.println("on-error");
+      }
+    }, 8);
+
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    int count = 0;
+    while(counter.get() != 4 && count < 10){
+      TimeUnit.SECONDS.sleep(1);
+      count++;
+    }
+    Assertions.assertEquals(4, counter.get());
+    observeRelation.proactiveCancel();
+    TimeUnit.SECONDS.sleep(1);
+    counter.set(0);
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    client.put("Test payload...".getBytes(), 0);
+    count = 0;
+    while(counter.get() == 0 && count < 2){
+      TimeUnit.SECONDS.sleep(1);
+      count++;
+    }
+    Assertions.assertEquals(0, counter.get());
+
     client.shutdown();
   }
 }
