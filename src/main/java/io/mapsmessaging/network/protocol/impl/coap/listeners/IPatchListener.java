4fedd8c984142cef60b0a04dbcfff3b9b08a3b74
Matthew Buckton <matthew@buckton.org>
2022-08-19 11:43:17 +1000

    Reject Patch requests since the server, currently, does not support such a thing

--- src/main/java/io/mapsmessaging/network/protocol/impl/coap/listeners/IPatchListener.java
+++ src/main/java/io/mapsmessaging/network/protocol/impl/coap/listeners/IPatchListener.java
@@ -1,26 +1,5 @@
 package io.mapsmessaging.network.protocol.impl.coap.listeners;
 
-import static io.mapsmessaging.network.protocol.impl.coap.packet.options.Constants.URI_PATH;
+public class IPatchListener extends PatchListener {
 
-import io.mapsmessaging.network.protocol.impl.coap.CoapProtocol;
-import io.mapsmessaging.network.protocol.impl.coap.packet.BasePacket;
-import io.mapsmessaging.network.protocol.impl.coap.packet.TYPE;
-import io.mapsmessaging.network.protocol.impl.coap.packet.options.OptionSet;
-import io.mapsmessaging.network.protocol.impl.coap.packet.options.UriPath;
-
-public class IPatchListener extends Listener {
-
-  @Override
-  public BasePacket handle(BasePacket request, CoapProtocol protocol) {
-    OptionSet optionSet = request.getOptions();
-    String path = "/";
-    if (optionSet.hasOption(URI_PATH)) {
-      UriPath uriPath = (UriPath) optionSet.getOption(URI_PATH);
-      path = uriPath.toString();
-    }
-    if (request.getType().equals(TYPE.CON)) {
-      // Need to create a response here!!!
-    }
-    return null;
-  }
 }
