c81c3d25ec2a501f6d52bd38ee7fa3e03f58f471
Matthew Buckton <matthew@buckton.org>
2022-10-20 17:30:27 +1100

    Return type as json Clean up the schema object

--- src/main/java/io/mapsmessaging/rest/impl/SchemaQueryApi.java
+++ src/main/java/io/mapsmessaging/rest/impl/SchemaQueryApi.java
@@ -4,17 +4,38 @@ import static spark.Spark.get;
 
 import io.mapsmessaging.engine.schema.SchemaManager;
 import io.mapsmessaging.rest.RestApi;
+import io.mapsmessaging.rest.data.SchemaData;
 import io.mapsmessaging.rest.transformation.JsonTransformation;
 import io.mapsmessaging.schemas.config.SchemaConfig;
+import java.util.ArrayList;
 import java.util.List;
+import spark.Request;
+import spark.Response;
 
 public class SchemaQueryApi implements RestApi {
 
   public void initialise() {
-    get("/schema/getAll", (req, res) -> getAll(), new JsonTransformation());
+    get("/schema/getAll", this::getAll, new JsonTransformation());
+    get("/schema/get/:id", this::getId, new JsonTransformation());
   }
 
-  private List<SchemaConfig> getAll() {
-    return SchemaManager.getInstance().getAll();
+  protected SchemaData getId(Request req, Response res) {
+    res.type("application/json");
+    String id =  req.params(":id");
+    for(SchemaConfig config:SchemaManager.getInstance().getAll()){
+      if(config.getUniqueId().equals(id)){
+        return new SchemaData(config);
+      }
+    }
+    return null;
+  }
+
+  protected List<SchemaData> getAll(Request req, Response res) {
+    res.type("application/json");
+    List<SchemaData> list = new ArrayList<>();
+    for(SchemaConfig config:SchemaManager.getInstance().getAll()){
+      list.add(new SchemaData(config));
+    }
+    return list;
   }
 }
--- src/main/java/io/mapsmessaging/rest/impl/ServerQueryApi.java
+++ src/main/java/io/mapsmessaging/rest/impl/ServerQueryApi.java
@@ -9,24 +9,28 @@ import io.mapsmessaging.rest.data.ProtocolInfo;
 import io.mapsmessaging.rest.transformation.JsonTransformation;
 import java.util.ArrayList;
 import java.util.List;
+import spark.Response;
 
 public class ServerQueryApi implements RestApi {
 
   public void initialise(){
-    get("/server/name", (req, res) -> getName());
-    get("/server/destinations", (req, res) -> getDestinations(), new JsonTransformation());
-    get("/server/protocols", (req, res) -> getProtocols(), new JsonTransformation());
+    get("/server/name", (req, res) -> getName(res));
+    get("/server/destinations", (req, res) -> getDestinations(res), new JsonTransformation());
+    get("/server/protocols", (req, res) -> getProtocols(res), new JsonTransformation());
   }
 
-  private String getName(){
+  private String getName(Response res) {
+    res.type("application/json");
     return MessageDaemon.getInstance().getId();
   }
 
-  private List<String> getDestinations(){
+  private List<String> getDestinations(Response res) {
+    res.type("application/json");
     return MessageDaemon.getInstance().getDestinationManager().getAll();
   }
 
-  private List<ProtocolInfo> getProtocols(){
+  private List<ProtocolInfo> getProtocols(Response res) {
+    res.type("application/json");
     List<EndPointManager> endPointManagers = MessageDaemon.getInstance().getNetworkManager().getAll();
     List<ProtocolInfo> protocols = new ArrayList<>();
     for (EndPointManager endPointManager : endPointManagers) {
