cff3aeb080d27319f544e3073fa8728509bcd393
Matthew Buckton <matthew@buckton.org>
2022-10-29 20:05:26 +1100

    update ping times

--- src/main/java/io/mapsmessaging/consul/Constants.java
+++ src/main/java/io/mapsmessaging/consul/Constants.java
@@ -20,8 +20,8 @@ package io.mapsmessaging.consul;
 
 public class Constants {
 
-  public static final long PING_TIME = 3;
-  public static final long HEALTH_TIME = 2;
+  public static final long PING_TIME = 30;
+  public static final long HEALTH_TIME = 20;
   public static final int CONSUL_PORT = 8080;
   public static final String NAME = "mapsMessaging";
   public static final String VERSION = "1.0";
