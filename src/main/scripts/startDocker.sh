f1424677ded3c74266118cef4107d0a05da6b574
Matthew Buckton <matthew@buckton.org>
2023-10-26 12:58:10 +1100

    pick up the consul host from the docker env

--- src/main/scripts/startDocker.sh
+++ src/main/scripts/startDocker.sh
@@ -1,27 +1,30 @@
 #
-#    Copyright [ 2020 - 2022 ] [Matthew Buckton]
+# Copyright [ 2020 - 2023 ] [Matthew Buckton]
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
-#
-#
-#
-# Define the home directory for the messaging daemon
 #
 export JAVA_HOME=/opt/jdk-13
 export PATH=$JAVA_HOME/bin:$PATH
 
 export VERSION=%%MAPS_VERSION%%
+
+# Get the value of the ConsulHost environment variable, or default to 127.0.0.1
+CONSUL_HOST="${ConsulHost:-127.0.0.1}"
+
+# Export it so that it's available to processes you might run from this script
+export CONSUL_HOST
+
 
 if [ -z ${MAPS_HOME+x} ];
   then export MAPS_HOME=/opt/message_daemon-$VERSION;
@@ -40,4 +43,4 @@ export CLASSPATH="$MAPS_CONF":$MAPS_LIB/message_daemon-$VERSION.jar:"$MAPS_LIB/*
 #
 # Now start the the daemon
 #
-java -classpath $CLASSPATH -DUSE_UUID=false -Djava.security.auth.login.config=$MAPS_CONF/jaasAuth.config -DMAPS_HOME=$MAPS_HOME io.mapsmessaging.MessageDaemon
+java -classpath $CLASSPATH -DUSE_UUID=false -DConsulHost=$CONSUL_HOST -Djava.security.auth.login.config=$MAPS_CONF/jaasAuth.config -DMAPS_HOME=$MAPS_HOME io.mapsmessaging.MessageDaemon
