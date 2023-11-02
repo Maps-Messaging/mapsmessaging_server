78152263d77029468872328e7e0bb2f543410f54
krital <alex@kritikal.org>
2023-11-02 11:36:32 +0200

    Check if FLY_CONSUL_URL is set and if so, parse it as needed to pass the required parts to the JVM as system properties

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -45,5 +45,33 @@ export LD_LIBRARY_PATH=$MAPS_LIB:$LD_LIBRARY_PATH
 #
 # Now start the the daemon
 #
-java -classpath $CLASSPATH $JAVA_OPTS -DConsulUrl=$CONSUL_URL -DConsulPath=$ConsulPath -Djava.security.auth.login.config=$MAPS_CONF/jaasAuth.config -DMAPS_HOME=$MAPS_HOME io.mapsmessaging.MessageDaemon
+if [ -z ${FLY_CONSUL_URL+x} ];
+then
+    echo "Detected fly.io consul URL ..."
+    protocol=$(echo "$FLY_CONSUL_URL" | grep "://" | sed -e's,^\(.*://\).*,\1,g')
+    # Remove the protocol
+    url_no_protocol=$(echo "${1/$protocol/}")
+    # Use tr: Make the protocol lower-case for easy string compare
+    protocol=$(echo "$protocol" | tr '[:upper:]' '[:lower:]')
+    # Extract the user and password (if any)
+    # cut 1: Remove the path part to prevent @ in the querystring from breaking the next cut
+    # rev: Reverse string so cut -f1 takes the (reversed) rightmost field, and -f2- is what we want
+    # cut 2: Remove the host:port
+    # rev: Undo the first rev above
+    userpass=$(echo "$url_no_protocol" | grep "@" | cut -d"/" -f1 | rev | cut -d"@" -f2- | rev)
+    pass=$(echo "$userpass" | grep ":" | cut -d":" -f2)
+    if [ -n "$pass" ]; then
+      user=$(echo "$userpass" | grep ":" | cut -d":" -f1)
+    else
+      user="$userpass"
+    fi
+    # Extract the host
+    hostport=$(echo "${url_no_protocol/$userpass@/}" | cut -d"/" -f1)
+    host=$(echo "$hostport" | cut -d":" -f1)
+    port=$(echo "$hostport" | grep ":" | cut -d":" -f2)
+    path=$(echo "$url_no_protocol" | grep "/" | cut -d"/" -f2-)
+    java -classpath $CLASSPATH $JAVA_OPTS -DUSE_UUID=false -DConsulUrl=$protocol$host -DConsulPath=$path -DConsulToken=$pass -Djava.security.auth.login.config=$MAPS_CONF/jaasAuth.config -DMAPS_HOME=$MAPS_HOME io.mapsmessaging.MessageDaemon
+else
+    java -classpath $CLASSPATH $JAVA_OPTS -DUSE_UUID=false -DConsulUrl=$CONSUL_URL -DConsulPath=$ConsulPath -Djava.security.auth.login.config=$MAPS_CONF/jaasAuth.config -DMAPS_HOME=$MAPS_HOME io.mapsmessaging.MessageDaemon
+fi
 
