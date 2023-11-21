226885029f17db1ebba3cc112a1f1984f7a9ea08
Matthew Buckton <matthew@buckton.org>
2023-11-21 15:47:10 +1100

    Ensure the json is in fact json now that we validate based on the schema

--- src/test/java/io/mapsmessaging/network/protocol/impl/stomp/StompSelectorTest.java
+++ src/test/java/io/mapsmessaging/network/protocol/impl/stomp/StompSelectorTest.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright [ 2020 - 2023 ] [Matthew Buckton]
  *
- *   Copyright [ 2020 - 2022 ] [Matthew Buckton]
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -21,17 +20,19 @@ package io.mapsmessaging.network.protocol.impl.stomp;
 import io.mapsmessaging.schemas.config.SchemaConfig;
 import io.mapsmessaging.schemas.config.impl.JsonSchemaConfig;
 import io.mapsmessaging.test.WaitForState;
+import net.ser1.stomp.Client;
+import net.ser1.stomp.Listener;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.security.auth.login.LoginException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
-import javax.security.auth.login.LoginException;
-import net.ser1.stomp.Client;
-import net.ser1.stomp.Listener;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 class StompSelectorTest extends StompBaseTest implements Listener {
 
@@ -52,7 +53,12 @@ class StompSelectorTest extends StompBaseTest implements Listener {
     client.send("$schema/"+topicName, config.pack());
     client.subscribeW(topicName, this, map);
     for(int x=0;x<EVENT_COUNT;x++){
-      String json = "{\"temperature\":28, \"count\":"+x+", \"testName\":simpleSelectorTest, \"odd\":"+x%2+"}";
+      JSONObject jsonObject = new JSONObject();
+      jsonObject.put("temperature", 28);
+      jsonObject.put("count", x);
+      jsonObject.put("testName", "simpleSelectorTest");
+      jsonObject.put("odd", x % 2);
+      String json = jsonObject.toString(2);
       map = new HashMap<>();
       map.put("receipt","message"+x);
       client.sendW(topicName, json, map);
@@ -69,15 +75,19 @@ class StompSelectorTest extends StompBaseTest implements Listener {
     Assertions.assertTrue(client.isConnected());
     Map<String, String> map = new HashMap<>();
     map.put("id", "subscribe1");
-    map.put("selector", "odd= 0");
+    map.put("selector", "odd = 0");
     SchemaConfig config = new JsonSchemaConfig();
     config.setUniqueId(UUID.randomUUID());
     String topicName = getTopicName();
     client.send("$schema/"+topicName, config.pack());
     client.subscribeW(topicName, this, map);
     for(int x=0;x<EVENT_COUNT;x++){
-      String json = "{\"temperature\":28, \"count\":"+x+", \"testName\":simpleSelectorTest, \"odd\":"+x%2+"}";
-      client.send(topicName, json);
+      JSONObject jsonObject = new JSONObject();
+      jsonObject.put("temperature", 28);
+      jsonObject.put("count", x);
+      jsonObject.put("testName", "simpleSelectorTest");
+      jsonObject.put("odd", x % 2);
+      client.send(topicName, jsonObject.toString(2));
     }
     map = new HashMap<>();
     map.put("receipt","message");
