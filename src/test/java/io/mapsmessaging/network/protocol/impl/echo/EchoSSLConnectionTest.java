ade217acc23a06215433c5fdc57fbc8aa8498750
Matthew Buckton <matthew@buckton.org>
2023-12-15 23:18:28 +1100

    set up the echo tests

--- src/test/java/io/mapsmessaging/network/protocol/impl/echo/EchoSSLConnectionTest.java
+++ src/test/java/io/mapsmessaging/network/protocol/impl/echo/EchoSSLConnectionTest.java
@@ -19,6 +19,7 @@
 package io.mapsmessaging.network.protocol.impl.echo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import io.mapsmessaging.test.BaseTestConfig;
