50099102713b3b81c0d6f4a54574325e8d7b85f8
Matthew Buckton <matthew@buckton.org>
2024-02-07 11:51:23 +1100

    parse mapping if it contains wildcards

--- src/main/java/io/mapsmessaging/network/protocol/impl/mqtt/listeners/PublishListener.java
+++ src/main/java/io/mapsmessaging/network/protocol/impl/mqtt/listeners/PublishListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [ 2020 - 2023 ] [Matthew Buckton]
+ * Copyright [ 2020 - 2024 ] [Matthew Buckton]
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -78,6 +78,16 @@ public class PublishListener extends PacketListener {
       lookup = map.get(publish.getDestinationName());
       if (lookup == null) {
         lookup = publish.getDestinationName();
+        for(Map.Entry<String, String> remote:map.entrySet()){
+          if(remote.getKey().endsWith("#")){
+            String check = remote.getValue();
+            String tmp = remote.getKey().substring(0, remote.getKey().length()-1);
+            if(lookup.startsWith(tmp)){
+              lookup = check + lookup;
+              lookup = lookup.replaceAll("//", "/");
+            }
+          }
+        }
       }
     }
     return lookup;
