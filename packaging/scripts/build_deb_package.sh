4267eb90af0d1183b1fb86a6b4264e6faec52969
Matthew Buckton <matthew@buckton.org>
2024-10-17 14:01:01 +1100

    try and fix build

--- packaging/scripts/build_deb_package.sh
+++ packaging/scripts/build_deb_package.sh
@@ -19,7 +19,7 @@
 
 export USER=$1
 export PASSWORD=$2
-export REPO_NAME=maps-apt-snapshot
+export REPO_NAME=maps_apt_snapshot
 
 # Extract POM version from pom.xml
 # Extract POM version from pom.xml
