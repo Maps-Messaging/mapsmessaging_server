e3d5e19be2d3034a9b440b0ee12d090735051be2
Matthew Buckton <matthew@buckton.org>
2024-10-17 14:58:41 +1100

    remove jersey json and implement google json read/writing for the rest api

---
+++ src/main/java/io/mapsmessaging/rest/translation/GsonMessageBodyReader.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright [ 2020 - 2024 ] [Matthew Buckton]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.mapsmessaging.rest.translation;
+
+import com.google.gson.Gson;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.ext.MessageBodyReader;
+import jakarta.ws.rs.ext.Provider;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
+@Provider
+@Consumes(MediaType.APPLICATION_JSON)
+public class GsonMessageBodyReader implements MessageBodyReader<Object> {
+
+  private final Gson gson = new Gson();
+
+  @Override
+  public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, jakarta.ws.rs.core.MediaType mediaType) {
+    return mediaType.equals(MediaType.APPLICATION_JSON_TYPE);
+  }
+
+  @Override
+  public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, jakarta.ws.rs.core.MediaType mediaType,
+                         jakarta.ws.rs.core.MultivaluedMap<String, String> httpHeaders, java.io.InputStream entityStream) {
+    return gson.fromJson(new InputStreamReader(entityStream, StandardCharsets.UTF_8), genericType);
+  }
+}
+
---
+++ src/main/java/io/mapsmessaging/rest/translation/GsonMessageBodyWriter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright [ 2020 - 2024 ] [Matthew Buckton]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.mapsmessaging.rest.translation;
+import com.google.gson.Gson;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.ext.MessageBodyWriter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class GsonMessageBodyWriter implements MessageBodyWriter<Object> {
+
+  private final Gson gson = new Gson();
+
+  @Override
+  public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, jakarta.ws.rs.core.MediaType mediaType) {
+    return mediaType.equals(MediaType.APPLICATION_JSON_TYPE);
+  }
+
+  @Override
+  public void writeTo(Object object, Class<?> type, Type genericType, Annotation[] annotations, jakarta.ws.rs.core.MediaType mediaType,
+                      jakarta.ws.rs.core.MultivaluedMap<String, Object> httpHeaders, java.io.OutputStream entityStream) throws IOException {
+    OutputStreamWriter writer = new OutputStreamWriter(entityStream, StandardCharsets.UTF_8);
+    gson.toJson(object, writer);
+    writer.flush();
+  }
+}
