62faac4143f5e072dd007ba5d51bc53cf543385e
Matthew Buckton <matthew@buckton.org>
2024-11-05 15:12:51 +1100

    update for actual amqp config parameters for the proton engine

--- src/main/java/io/mapsmessaging/config/protocol/impl/AmqpConfig.java
+++ src/main/java/io/mapsmessaging/config/protocol/impl/AmqpConfig.java
@@ -30,12 +30,35 @@ import lombok.ToString;
 @ToString
 public class AmqpConfig extends ProtocolConfig {
 
+  private int idleTimeout = 30000; // Default 30 seconds
+  private int maxFrameSize = 65536; // Default 64KB
+  private int linkCredit = 50; // Default link credit
+  private boolean durable = false; // Link durability
+  private int incomingCapacity = 65536; // Session incoming capacity
+  private int outgoingWindow = 100; // Session outgoing window
+
   public AmqpConfig(ConfigurationProperties config) {
     super(config);
     setType("amqp");
+
+    // Initialize fields from config
+    this.idleTimeout = config.getIntProperty("idleTimeout", idleTimeout);
+    this.maxFrameSize = config.getIntProperty("maxFrameSize", maxFrameSize);
+    this.linkCredit = config.getIntProperty("linkCredit", linkCredit);
+    this.durable = config.getBooleanProperty("durable", durable);
+    this.incomingCapacity = config.getIntProperty("incomingCapacity", incomingCapacity);
+    this.outgoingWindow = config.getIntProperty("outgoingWindow", outgoingWindow);
   }
 
+  @Override
   public ConfigurationProperties toConfigurationProperties() {
-    return super.toConfigurationProperties();
+    ConfigurationProperties properties = super.toConfigurationProperties();
+    properties.put("idleTimeout", this.idleTimeout);
+    properties.put("maxFrameSize", this.maxFrameSize);
+    properties.put("linkCredit", this.linkCredit);
+    properties.put("durable", this.durable);
+    properties.put("incomingCapacity", this.incomingCapacity);
+    properties.put("outgoingWindow", this.outgoingWindow);
+    return properties;
   }
 }
