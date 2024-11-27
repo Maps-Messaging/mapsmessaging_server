9caea373c9e46959d78c75b8dfb6ce58803ce702
Matthew Buckton <matthew@buckton.org>
2024-11-27 14:32:10 +1100

    sort the response and limit the size

--- src/main/java/io/mapsmessaging/rest/api/impl/destination/DestinationManagementApi.java
+++ src/main/java/io/mapsmessaging/rest/api/impl/destination/DestinationManagementApi.java
@@ -24,6 +24,7 @@ import static io.mapsmessaging.rest.api.Constants.URI_PATH;
 import io.mapsmessaging.MessageDaemon;
 import io.mapsmessaging.dto.helpers.DestinationStatusHelper;
 import io.mapsmessaging.dto.rest.destination.DestinationDTO;
+import io.mapsmessaging.rest.cache.CacheKey;
 import io.mapsmessaging.rest.responses.DestinationResponse;
 import io.mapsmessaging.selector.ParseException;
 import io.mapsmessaging.selector.SelectorParser;
@@ -52,35 +53,91 @@ public class DestinationManagementApi extends BaseDestinationApi {
       return null;
     }
 
+    CacheKey key = new CacheKey(uriInfo.getPath(), destinationName);
+
+    // Try to retrieve from cache
+    DestinationDTO cachedResponse = getFromCache(key, DestinationDTO.class);
+    if (cachedResponse != null) {
+      return cachedResponse;
+    }
     DestinationDTO destination = DestinationStatusHelper.createDestination(lookup(destinationName));
     if(destination == null) {
       throw new WebApplicationException("Destination not found", Response.Status.NOT_FOUND);
     }
+    putToCache(key, destination);
     return destination;
   }
 
   @GET
   @Path("/server/destinations")
   @Produces({MediaType.APPLICATION_JSON})
-  //@ApiOperation(value = "Get all the destination configuration")
-  public DestinationResponse getAllDestinations(@QueryParam("filter") String filter) throws ExecutionException, InterruptedException, TimeoutException, ParseException {
+  public DestinationResponse getAllDestinations(
+      @QueryParam("filter") String filter,
+      @QueryParam("size") @DefaultValue("40") int size,
+      @QueryParam("sortBy") @DefaultValue("Published") String sortBy)
+      throws ExecutionException, InterruptedException, TimeoutException, ParseException {
+
     checkAuthentication();
     if (!hasAccess("destinations")) {
       response.setStatus(403);
       return null;
     }
-    ParserExecutor parser = (filter != null && !filter.isEmpty())  ? SelectorParser.compile(filter) : null;
+
+    // Create cache key
+    CacheKey key = new CacheKey(uriInfo.getPath(), ((filter != null && !filter.isEmpty()) ? ""+filter.hashCode() : "")+":"+sortBy+":"+size );
+
+    // Try to retrieve from cache
+    DestinationResponse cachedResponse = getFromCache(key, DestinationResponse.class);
+    if (cachedResponse != null) {
+      return cachedResponse;
+    }
+
+
+    ParserExecutor parser = (filter != null && !filter.isEmpty()) ? SelectorParser.compile(filter) : null;
     List<String> destinations = MessageDaemon.getInstance().getDestinationManager().getAll();
-    List<DestinationDTO> results  = new ArrayList<>();
-    for(String name:destinations){
+    List<DestinationDTO> results = new ArrayList<>();
+
+    for (String name : destinations) {
       DestinationDTO destination = DestinationStatusHelper.createDestination(lookup(name));
-      if(parser == null || parser.evaluate(destination)) {
-        results.add(destination);
+      if (parser == null || parser.evaluate(destination)) {
+        if (destination != null) {
+          results.add(destination);
+        }
       }
     }
-    return new DestinationResponse(request, results);
+
+    // Sort by specified field
+    results.sort((d1, d2) -> {
+      switch (sortBy) {
+        case "Name":
+          return d2.getName().compareTo(d1.getName());
+        case "Published":
+          return Long.compare(d2.getPublishedMessages(), d1.getPublishedMessages());
+        case "Delivered":
+          return Long.compare(d2.getDeliveredMessages(), d1.getDeliveredMessages());
+        case "Stored":
+          return Long.compare(d2.getStoredMessages(), d1.getStoredMessages());
+        case "Pending":
+          return Long.compare(d2.getPendingMessages(), d1.getPendingMessages());
+        case "Delayed":
+          return Long.compare(d2.getDelayedMessages(), d1.getDelayedMessages());
+        case "Expired":
+          return Long.compare(d2.getExpiredMessages(), d1.getExpiredMessages());
+        default:
+          throw new IllegalArgumentException("Invalid sortBy parameter: " + sortBy);
+      }
+    });
+
+
+    // Limit the size of the returned results
+    if (size > 0 && size < results.size()) {
+      results = results.subList(0, size);
+    }
+    for(DestinationDTO destination : results) {
+      System.out.println(destination.getName()+":"+destination.getStoredMessages());
+    }
+    DestinationResponse destinationResponse =  new DestinationResponse(request, results);
+    putToCache(key, destinationResponse);
+    return destinationResponse;
   }
-
-
-
 }
