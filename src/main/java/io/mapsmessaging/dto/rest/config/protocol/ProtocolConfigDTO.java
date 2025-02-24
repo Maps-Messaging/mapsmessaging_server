2005f6fad999cb023c689c05833338e2a53a769e
Matthew Buckton <matthew@buckton.org>
2025-02-24 16:02:52 +1100

    ensure the json is correctly streamed

--- src/main/java/io/mapsmessaging/dto/rest/config/protocol/ProtocolConfigDTO.java
+++ src/main/java/io/mapsmessaging/dto/rest/config/protocol/ProtocolConfigDTO.java
@@ -40,7 +40,7 @@ import lombok.NoArgsConstructor;
     @JsonSubTypes.Type(value = SemtechConfigDTO.class, name = "semtech"),
     @JsonSubTypes.Type(value = StompConfigDTO.class, name = "stomp"),
     @JsonSubTypes.Type(value = WebSocketConfigDTO.class, name = "websocket"),
-    @JsonSubTypes.Type(value = ExtensionConfigDTO.class, name = "plugin")
+    @JsonSubTypes.Type(value = ExtensionConfigDTO.class, name = "extension")
 })
 @Schema(
     description = "Abstract base class for all protocol configurations",
@@ -56,7 +56,7 @@ import lombok.NoArgsConstructor;
         @DiscriminatorMapping(value = "semtech", schema = SemtechConfigDTO.class),
         @DiscriminatorMapping(value = "stomp", schema = StompConfigDTO.class),
         @DiscriminatorMapping(value = "websocket", schema = WebSocketConfigDTO.class),
-        @DiscriminatorMapping(value = "plugin", schema = ExtensionConfigDTO.class)
+        @DiscriminatorMapping(value = "extension", schema = ExtensionConfigDTO.class)
 
     },
     requiredProperties = {"type"}
@@ -67,7 +67,7 @@ import lombok.NoArgsConstructor;
 public abstract class ProtocolConfigDTO extends BaseConfigDTO {
 
   @Schema(description = "Type of the protocol configuration", allowableValues = {
-      "amqp", "coap", "lora", "mqtt", "mqtt-sn", "mqttV5", "nmea", "semtech", "stomp", "websocket", "plugin"
+      "amqp", "coap", "lora", "mqtt", "mqtt-sn", "mqttV5", "nmea", "semtech", "stomp", "websocket", "extension"
   })
   protected String type;
 
--- src/main/java/io/mapsmessaging/dto/rest/config/protocol/impl/ExtensionConfigDTO.java
+++ src/main/java/io/mapsmessaging/dto/rest/config/protocol/impl/ExtensionConfigDTO.java
@@ -2,11 +2,13 @@ package io.mapsmessaging.dto.rest.config.protocol.impl;
 
 import io.mapsmessaging.dto.rest.config.protocol.ProtocolConfigDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
+@EqualsAndHashCode(callSuper=false)
 public class ExtensionConfigDTO extends ProtocolConfigDTO {
 
   @Schema(description = "Map of config entries")
