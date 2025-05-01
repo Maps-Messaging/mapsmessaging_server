8a5fe4993158073c174a95b6d866e4bbb4e57920
Matthew Buckton <matthew@buckton.org>
2025-05-01 14:39:08 +1000

    jetstream framework, next need to wire in the integration with the maps engine. There is a 1:1 with either MQTT V5 or AMQP, so the engine can process this

---
+++ src/main/java/io/mapsmessaging/network/protocol/impl/nats/jetstream/stream/handlers/StreamDeleteHandler.java
@@ -0,0 +1,21 @@
+package io.mapsmessaging.network.protocol.impl.nats.jetstream.stream.handlers;
+
+import io.mapsmessaging.network.protocol.impl.nats.frames.NatsFrame;
+import io.mapsmessaging.network.protocol.impl.nats.frames.PayloadFrame;
+import io.mapsmessaging.network.protocol.impl.nats.jetstream.stream.JetStreamHandler;
+import io.mapsmessaging.network.protocol.impl.nats.state.SessionState;
+
+import java.io.IOException;
+
+public class StreamDeleteHandler implements JetStreamHandler {
+
+  @Override
+  public String getName() {
+    return "DELETE";
+  }
+
+  @Override
+  public NatsFrame handle(PayloadFrame frame, SessionState sessionState) throws IOException {
+    return null;
+  }
+}
