55cea884e1147d722c6ff1a179c3b6730297750f
Matthew Buckton <matthew@buckton.org>
2025-06-13 15:02:30 +1000

    do not mix mqtt-sn and mqtt

--- src/main/java/io/mapsmessaging/network/protocol/ProtocolFactory.java
+++ src/main/java/io/mapsmessaging/network/protocol/ProtocolFactory.java
@@ -50,7 +50,7 @@ public class ProtocolFactory implements ServiceManager {
 
   public ProtocolImplFactory getBoundedProtocol() {
     for (ProtocolImplFactory protocol : protocolServiceList) {
-      if (protocols.contains(protocol.getName().toLowerCase())) {
+      if (protocols.equalsIgnoreCase(protocol.getName())) {
         return protocol;
       }
     }
