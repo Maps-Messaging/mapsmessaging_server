4a0b634adabda2cde1db181507915665c9cad99e
Matthew Buckton <matthew@buckton.org>
2025-06-19 17:16:59 +1000

    simplify the reaper queue

--- src/main/java/io/mapsmessaging/utilities/queue/EventReaperQueue.java
+++ src/main/java/io/mapsmessaging/utilities/queue/EventReaperQueue.java
@@ -22,64 +22,37 @@ package io.mapsmessaging.utilities.queue;
 import io.mapsmessaging.utilities.collections.NaturalOrderedLongQueue;
 import io.mapsmessaging.utilities.collections.bitset.BitSetFactoryImpl;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class EventReaperQueue {
 
-  private final EventQueue[] localQueue;
-  private final AtomicLong index;
+  private Queue<Long> localQueue;
+  private int queueSize;
 
   public EventReaperQueue() {
-    index = new AtomicLong(0);
-    int numberOfCPUs = Runtime.getRuntime().availableProcessors();
-    localQueue = new EventQueue[numberOfCPUs];
-    for (int x = 0; x < numberOfCPUs; x++) {
-      localQueue[x] = new EventQueue();
-    }
+    queueSize = 512;
+    localQueue = new NaturalOrderedLongQueue(0, new BitSetFactoryImpl(queueSize));
   }
 
-  public void close() {
-    for (EventQueue threadLocalQueue : localQueue) {
-      threadLocalQueue.getAndClear();
-    }
+  public synchronized void close() {
+    localQueue.clear();
   }
 
-  public void add(long id) {
-    int idx = (int) index.incrementAndGet() % localQueue.length;
-    localQueue[idx].add(id);
+  public synchronized void add(long id) {
+    localQueue.add(id);
   }
 
-  public Queue<Long> getAndClear() {
-    if (Arrays.stream(localQueue).anyMatch(threadLocalQueue -> !threadLocalQueue.queue.isEmpty())) {
-      Queue<Long> result = new NaturalOrderedLongQueue(0, new BitSetFactoryImpl(8192));
-      for (EventQueue threadLocalQueue : localQueue) {
-        Queue<Long> tQueue = threadLocalQueue.getAndClear();
-        result.addAll(tQueue);
+  public synchronized Queue<Long> getAndClear() {
+    if(!localQueue.isEmpty()) {
+      if(localQueue.size() > queueSize && queueSize < 4096) {
+        queueSize = queueSize*2;
       }
-      return result;
+      Queue<Long> copy = localQueue;
+      localQueue = new NaturalOrderedLongQueue(0, new BitSetFactoryImpl(queueSize));
+      return copy;
     }
     return new LinkedList<>();
   }
 
-  private static final class EventQueue {
-
-    private Queue<Long> queue;
-
-    public EventQueue() {
-      queue = new NaturalOrderedLongQueue(0, new BitSetFactoryImpl(8192));
-    }
-
-    public synchronized void add(long entry) {
-      queue.offer(entry);
-    }
-
-    public synchronized Queue<Long> getAndClear() {
-      Queue<Long> copy = queue;
-      queue = new NaturalOrderedLongQueue(0, new BitSetFactoryImpl(8192));
-      return copy;
-    }
-  }
 }
