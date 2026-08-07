f875f6702df28b30e824e79b0c3ea6ece9cef293
Matthew Buckton <matthew@buckton.org>
2026-08-07 15:22:41 +1000

    test(network): preserve DTLS client certificate policy
    NO-ISSUE

---
+++ src/test/java/io/mapsmessaging/network/io/impl/dtls/DTLSSessionManagerTest.java
@@ -0,0 +1,54 @@
+/*
+ *
+ *  Copyright [ 2020 - 2024 ] Matthew Buckton
+ *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
+ *
+ *  Licensed under the Apache License, Version 2.0 with the Commons Clause
+ *  (the "License"); you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://commonsclause.com/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.mapsmessaging.network.io.impl.dtls;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DTLSSessionManagerTest {
+
+  @Test
+  void dtls_parameters_preserve_required_client_certificates() throws Exception {
+    SSLEngine sslEngine = createEngine();
+    sslEngine.setNeedClientAuth(true);
+
+    DTLSSessionManager.configureEngine(sslEngine);
+
+    Assertions.assertTrue(sslEngine.getNeedClientAuth());
+  }
+
+  @Test
+  void dtls_parameters_preserve_wanted_client_certificates() throws Exception {
+    SSLEngine sslEngine = createEngine();
+    sslEngine.setWantClientAuth(true);
+
+    DTLSSessionManager.configureEngine(sslEngine);
+
+    Assertions.assertTrue(sslEngine.getWantClientAuth());
+  }
+
+  private SSLEngine createEngine() throws Exception {
+    SSLContext context = SSLContext.getInstance("DTLS");
+    context.init(null, null, null);
+    return context.createSSLEngine();
+  }
+}
