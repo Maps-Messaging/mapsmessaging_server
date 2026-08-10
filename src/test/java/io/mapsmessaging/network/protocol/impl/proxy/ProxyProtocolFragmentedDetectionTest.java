37998fb3d5c854c25afb6820089bb9031b1ad16b
matthew buckton <matthew.buckton@mapsmessaging.io>
2026-08-10 19:46:16 +1000

    fix copyright

--- src/test/java/io/mapsmessaging/network/protocol/impl/proxy/ProxyProtocolFragmentedDetectionTest.java
+++ src/test/java/io/mapsmessaging/network/protocol/impl/proxy/ProxyProtocolFragmentedDetectionTest.java
@@ -3,7 +3,7 @@
  *  Copyright [ 2020 - 2024 ] Matthew Buckton
  *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
  *
- *  Licensed under the Apache License, Version 2.0 with the Commonsclause
+ *  Licensed under the Apache License, Version 2.0 with the Commons Clause
  *  (the "License"); you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at:
  *
