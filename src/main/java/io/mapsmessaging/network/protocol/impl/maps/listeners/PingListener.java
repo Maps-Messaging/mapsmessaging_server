1ff31d750e9e6b92460c24cc9c4fbc37972a2845
Matthew Buckton <matthew@buckton.org>
2026-08-28 17:42:21 +1000

    feat(maps): add ping listener

---
+++ src/main/java/io/mapsmessaging/network/protocol/impl/maps/listeners/PingListener.java
@@ -0,0 +1,12 @@
+/* Copyright [ 2020 - 2026 ] MapsMessaging B.V. */
+package io.mapsmessaging.network.protocol.impl.maps.listeners;
+
+import io.mapsmessaging.network.protocol.impl.maps.MapsFrame;
+import io.mapsmessaging.network.protocol.impl.maps.MapsProtocol;
+
+public class PingListener implements MapsPacketListener {
+  @Override
+  public void handle(MapsFrame frame, MapsProtocol protocol) {
+    protocol.handlePing(frame);
+  }
+}
