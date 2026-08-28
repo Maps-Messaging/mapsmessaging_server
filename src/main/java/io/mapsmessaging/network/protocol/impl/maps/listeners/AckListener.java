3a8e2928909a36374a868008e85d1baac4f25e1f
Matthew Buckton <matthew@buckton.org>
2026-08-28 17:42:15 +1000

    feat(maps): add ack listener

---
+++ src/main/java/io/mapsmessaging/network/protocol/impl/maps/listeners/AckListener.java
@@ -0,0 +1,13 @@
+/* Copyright [ 2020 - 2026 ] MapsMessaging B.V. */
+package io.mapsmessaging.network.protocol.impl.maps.listeners;
+
+import io.mapsmessaging.network.protocol.impl.maps.MapsFrame;
+import io.mapsmessaging.network.protocol.impl.maps.MapsProtocol;
+import java.io.IOException;
+
+public class AckListener implements MapsPacketListener {
+  @Override
+  public void handle(MapsFrame frame, MapsProtocol protocol) throws IOException {
+    protocol.handleAck(frame);
+  }
+}
