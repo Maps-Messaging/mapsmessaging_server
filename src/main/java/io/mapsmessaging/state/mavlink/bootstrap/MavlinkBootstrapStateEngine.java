6de771da9f54345ded0beab01a32c4aa1f47c03e
Matthew Buckton <matthew@buckton.org>
2026-09-15 22:42:08 +0100

    fix(mavlink): retry bootstrap immediately after timeout
    Refs: MSG-303

--- src/main/java/io/mapsmessaging/state/mavlink/bootstrap/MavlinkBootstrapStateEngine.java
+++ src/main/java/io/mapsmessaging/state/mavlink/bootstrap/MavlinkBootstrapStateEngine.java
@@ -4,17 +4,17 @@
  *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
  *
  *  Licensed under the Apache License, Version 2.0 with the Commons Clause
- *  (the "License"); you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at:
+ * (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *      https://commonsclause.com/
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://commonsclause.com/
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.mapsmessaging.state.mavlink.bootstrap;
@@ -225,6 +225,7 @@ public class MavlinkBootstrapStateEngine {
               "Timed out waiting for " + requestTracker.getMissingState()
           )
       );
+      return true;
     }
 
     return retryDue(requestTracker, now, bootstrapProfile.getRecoveryRetryInterval());
