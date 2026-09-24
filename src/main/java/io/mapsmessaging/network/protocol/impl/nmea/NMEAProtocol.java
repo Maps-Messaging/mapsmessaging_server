a0cb63874501b84ad60718d8c25ffa3ece86e77d
Matthew Buckton <matthew@buckton.org>
2026-09-24 15:22:48 +0100

    fix(nmea): return on incomplete stream sentence
    Refs: MSG-335

--- src/main/java/io/mapsmessaging/network/protocol/impl/nmea/NMEAProtocol.java
+++ src/main/java/io/mapsmessaging/network/protocol/impl/nmea/NMEAProtocol.java
@@ -161,8 +161,10 @@ public class NMEAProtocol extends Protocol {
         if(sentenceId != null && !sentenceId.isEmpty()) {
           prepareSentence(nmeaPacket.getSentence(), sentenceId, nmeaPacket.getEntries());
         }
-        } catch (EndOfBufferException e) {
+      } catch (EndOfBufferException e) {
         packet.position(pos);
+        endPoint.register(SelectionKey.OP_READ, selectorTask.getReadTask());
+        return false;
       }
     }
     endPoint.register(SelectionKey.OP_READ, selectorTask.getReadTask());
