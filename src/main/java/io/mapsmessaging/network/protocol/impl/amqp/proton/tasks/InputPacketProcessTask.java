e366e69f069c1fc5c7da07f3b471150b506a646f
Matthew Buckton <matthew@buckton.org>
2026-09-24 15:22:30 +0100

    fix(amqp): leave shared read packet lifecycle to read task
    Refs: MSG-335

--- src/main/java/io/mapsmessaging/network/protocol/impl/amqp/proton/tasks/InputPacketProcessTask.java
+++ src/main/java/io/mapsmessaging/network/protocol/impl/amqp/proton/tasks/InputPacketProcessTask.java
@@ -91,9 +91,6 @@ public class InputPacketProcessTask extends PacketTask {
       }
     }
     processOutput();
-    if (!incomingPacket.hasRemaining()) {
-      incomingPacket.clear();
-    }
   }
 
 
