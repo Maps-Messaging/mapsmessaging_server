f6265905d8549e1e59b857de40cc2988a499aedd
Matthew Buckton <matthew@buckton.org>
2026-09-25 14:28:59 +0100

    refactor(session): isolate subscription controller construction
    Refs: MSG-339

---
+++ src/main/java/io/mapsmessaging/engine/session/SubscriptionControllerFactory.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  Copyright [ 2020 - 2024 ] Matthew Buckton
+ *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
+ *
+ *  Licensed under the Apache License, Version 2.0 with the Commons Clause
+ *  (the "License"); you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://commonsclause.com/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.mapsmessaging.engine.session;
+
+import io.mapsmessaging.engine.destination.DestinationManager;
+import io.mapsmessaging.engine.destination.subscription.SubscriptionContext;
+import io.mapsmessaging.engine.destination.subscription.SubscriptionController;
+import io.mapsmessaging.engine.session.persistence.SessionDetails;
+
+import java.util.Map;
+
+class SubscriptionControllerFactory {
+
+  SubscriptionController create(SessionContext context, DestinationManager destinationManager, Map<String, SubscriptionContext> subscriptions) {
+    return new SubscriptionController(context, destinationManager, subscriptions);
+  }
+
+  SubscriptionController create(String sessionId, SessionDetails sessionDetails, DestinationManager destinationManager,
+      Map<String, SubscriptionContext> subscriptions) {
+    return new SubscriptionController(sessionId, sessionDetails, destinationManager, subscriptions);
+  }
+}
